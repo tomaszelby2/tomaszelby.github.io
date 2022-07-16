3c2a51ebf4224c7cd332350ba1a369ca3989ec0f
tomaszelby2 <itakwygraizak@gmail.com>
2022-07-16 19:39:50 +0200

    Some animations, another changing css files

--- index.js
+++ index.js
@@ -2,7 +2,31 @@ function showPopupItem1() {
     let popup = document.getElementById("popup-project-item-1");
     popup.classList.replace("popup-invisible","popup-visible");
 }
-function closePopup() {
+function closePopup1() {
     let popup = document.getElementById("popup-project-item-1");
+    popup.classList.replace("popup-visible","popup-invisible");
+}
+function showPopupItem2() {
+    let popup = document.getElementById("popup-project-item-2");
+    popup.classList.replace("popup-invisible","popup-visible");
+}
+function closePopup2() {
+    let popup = document.getElementById("popup-project-item-2");
+    popup.classList.replace("popup-visible","popup-invisible");
+}
+function showPopupItem3() {
+    let popup = document.getElementById("popup-project-item-3");
+    popup.classList.replace("popup-invisible","popup-visible");
+}
+function closePopup3() {
+    let popup = document.getElementById("popup-project-item-3");
+    popup.classList.replace("popup-visible","popup-invisible");
+}
+function showPopupItem4() {
+    let popup = document.getElementById("popup-project-item-4");
+    popup.classList.replace("popup-invisible","popup-visible");
+}
+function closePopup4() {
+    let popup = document.getElementById("popup-project-item-4");
     popup.classList.replace("popup-visible","popup-invisible");
 }
